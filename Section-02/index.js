ea9ed703086476bef34deaba5040ead7d39ae4eb
Meargteame <mearegteame99995555@gmail.com>
2024-09-07 21:35:06 +0300

    types and conditionals

---
+++ Section-02/index.js
@@ -0,0 +1,77 @@
+// const preferDarkMode =true;
+// const preferSolarizedMode = true;
+
+
+// if(preferDarkMode){
+//     console.log('Dark mode set !')
+//     document.body.style.background ='black'
+// }
+// else if(preferSolarizedMode){
+//     console.log('Solarized mode set!')
+//     document.body.style.background ='palegoldenrod'
+// }
+
+// else{
+//     console.log('Light Mode set!')
+// }
+
+
+// const colorMode = 'solarized';
+
+// if (colorMode === 'dark'){
+//     console.log('Dark Mode is set!')
+//     document.body.style.background ='black'
+// }
+// else if(colorMode === 'solarized'){
+//     console.log('Solarized Mode is set!')
+//     document.body.style.background ='palegoldenrod'
+// }
+// else{
+//     console.log('Light Mode is set!')
+// }
+
+// switch to the case statements
+// const colorMode ='dark'
+
+// switch (colorMode){
+//     case 'dark':
+//         document.body.style.background ='black'
+//         break;
+//     case 'solarized':
+//         document.body.style.background ='palegoldenrod'
+//         break;
+//     default:
+//         document.body.style.background ='white'
+// }
+
+// Challenge 1: create a conditional that logs out "Good morning!", "Good evening!" and so on depending on the value of time (<- a variable)
+
+// Challenge 2: Rewrite the whole thing as a switch statement
+
+// const time = "morning" // could be "afternoon", "evening" and "night"
+
+// if (time === 'morning'){
+//     console.log('Good morning!')
+// }
+// else if(time === 'afternoon'){
+//     console.log('Good afternoon!')
+// }
+// else {
+//     console.log('Good evening!')
+// }
+
+
+// const time = "evening" // could be "afternoon", "evening" and "night"
+
+// // switch to the case 
+
+// switch(time){
+//     case 'morning':
+//         console.log('Good morning!')
+//         break;
+//     case 'afternoon':
+//         console.log('Good afternoon')
+//         break;
+//     default:
+//         console.log('Good evening!')
+// }
